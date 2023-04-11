ed9a4751d8d7bfd0fc98ac8ababf0087cd8a5085
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-04-12 01:23:39 +0900

    implement: バージョン等、ステータス値をフッターに表示する。

--- webapp/resources/views/users/layout.blade.php
+++ webapp/resources/views/users/layout.blade.php
@@ -9,5 +9,9 @@
     @yield('content')
 </div>
     
+<div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
+    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+</div>
+
 </body>
 </html>
