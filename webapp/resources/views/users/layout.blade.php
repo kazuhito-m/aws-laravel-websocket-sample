10c1f3b0c61444132f70a382053a4a9ddf820c2d
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-04-18 00:00:31 +0900

    implement: 今まで作ってたページの見栄えを整える。

--- webapp/resources/views/users/layout.blade.php
+++ webapp/resources/views/users/layout.blade.php
@@ -1,17 +1,23 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Laravel 10 CRUD Application - kazuhito_m</title>
-</head>
-<body>
-    
-<div class="container">
-    @yield('content')
-</div>
-    
-<div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
-</div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Profile') }}
+        </h2>
+    </x-slot>
 
-</body>
-</html>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    <div class="container">
+                        @yield('content')
+                    </div>                
+                </div>
+            </div>
+        </div>
+    </div>
+        
+    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+    </div>    
+</x-app-layout>
