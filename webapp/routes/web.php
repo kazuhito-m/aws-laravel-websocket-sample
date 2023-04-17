60243919e8bff0c57875412bbd303c9e7938d230
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-04-17 23:49:20 +0900

    implement: Userのページに認証をかけるように。

--- webapp/routes/web.php
+++ webapp/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,5 +26,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('users', UserController::class)
+    ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
