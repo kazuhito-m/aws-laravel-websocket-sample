49f91f623bf4138e6ce44812a5bc2a9327205e80
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-04-24 22:01:33 +0900

    bugfix: Route設定では動かないので(良くないけど)生のPathでリンク貼る。

--- webapp/resources/views/dashboard.blade.php
+++ webapp/resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="pull-right">
-                    <a class="btn btn-success" href="{{ route('users') }}"> Users</a>
+                    <a class="btn btn-success" href="/users"> Users</a>
                 </div>    
             </div>
         </div>
