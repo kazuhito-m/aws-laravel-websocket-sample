eecd38f67d06c15134215cece99a8ad4a39761c5
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-04-28 22:45:11 +0900

    settings: Dockerビルド時には、かならずキャッシュを使わないように。

--- build-test.sh
+++ build-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# docker build --no-cache -t laravel-test:latest . \
-docker build -t laravel-test:latest . \
+# docker build -t laravel-test:latest . \
+docker build --no-cache -t laravel-test:latest . \
 && docker run -p 80:80 --name la-test laravel-test:latest
  
 docker rm -f la-test
