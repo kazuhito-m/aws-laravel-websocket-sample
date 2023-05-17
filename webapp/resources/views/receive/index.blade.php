73dc69dfbb7052fd8faaec935bd28dc5720443a2
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-05-17 23:00:17 +0900

    implement: 設定ファイルの値を使って、HTML「にパラメータ埋める。

--- webapp/resources/views/receive/index.blade.php
+++ webapp/resources/views/receive/index.blade.php
@@ -76,7 +76,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl" id="userIdentityPart" data-user-id="{{ Auth::user()->id }}">
+                <div class="max-w-xl" id="userIdentityPart"
+                    data-user-id="{{ Auth::user()->id }}"
+                    data-websocket-url="{{ config('costom.websocket-url') }}">
                     User: {{ Auth::user()->name }}({{ Auth::user()->id }}) へのメッセージを表示する画面です。
                 </div>
 
