0d307574fb2e9ae64a70b4a914821590d7fc5556
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-05-18 04:22:03 +0900

    implement: とりあえず「外部APIに対して固定値送る」実装だけを作ってみる。

--- webapp/app/Http/Controllers/SendController.php
+++ webapp/app/Http/Controllers/SendController.php
@@ -23,9 +23,20 @@ class SendController extends Controller
 
         // TODO APIに送信
         Log::debug('ID:' . $request->get('id') . ', message:' . $request->get('message'));
-        // Log::debug($request);
+
+        $url = config('custom.client-send-api-url');
+        $options = array(
+            'http' => array(
+                'method'=> 'POST',
+                'header'=> 'Content-type: application/json; charset=UTF-8',
+                'content' => '{ "toUserId": "1", "message": "サーバのボタンで登録できた結果ですわ。", "fromUserId": "2", "fromUserName": "Kazuhito Miura", "fromServerTime": "2023/12/11 00:11:22"}'
+            )
+        );
+        $context = stream_context_create($options);
+        $contents = file_get_contents($url, false,$context);
 
         return redirect()->route('send.index')
             ->with('success', '送信成功しました。');
     }
+
 }
