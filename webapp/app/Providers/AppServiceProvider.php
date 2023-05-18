267f431b0cc1176ae8135cb723866da03fd25d51
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-05-18 16:51:26 +0900

    implement: 設定が足らず、ALBでアセット群がHTTPSに成ってくれなかったので、追加設定。

--- webapp/app/Providers/AppServiceProvider.php
+++ webapp/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (request()->isSecure()) URL::forceScheme('https');
     }
 }
