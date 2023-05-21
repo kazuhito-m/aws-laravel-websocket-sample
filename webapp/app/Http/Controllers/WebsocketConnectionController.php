0322fdc22e235906e51213390fcce950f904c985
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-05-21 15:12:55 +0900

    implement: 一覧ページを一通り実装。

--- webapp/app/Http/Controllers/WebsocketConnectionController.php
+++ webapp/app/Http/Controllers/WebsocketConnectionController.php
@@ -5,17 +5,23 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreWebsocketConnectionRequest;
 use App\Http\Requests\UpdateWebsocketConnectionRequest;
 use App\Models\Websocket\WebsocketConnection;
+use Illuminate\Support\Facades\Log;
 
 class WebsocketConnectionController extends Controller
 {
     public function index()
     {
-        $connections = WebsocketConnection::all();
-        return view('user.index')->with('websocketConnection', $websocketConnection);
+        $websocketConnections = WebsocketConnection::all();
+        return view('websocketconnection.index')->with('websocketConnections', $websocketConnections);
     }
 
     public function destroy(WebsocketConnection $websocketConnection)
     {
-        //
+        Log::debug('削除対象ID:' . $websocketConnection->id);
+
+        $websocketConnection->delete();
+
+        return redirect()->route('websocketconnections.index')
+            ->with('success', 'websocket connections deleted successfully');
     }
 }
--- webapp/routes/web.php
+++ webapp/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ReceiveController;
 use App\Http\Controllers\SendController;
 use App\Http\Controllers\DirectSendController;
+use App\Http\Controllers\WebsocketConnectionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -34,5 +35,6 @@ Route::resource('users', UserController::class)->middleware(['auth', 'verified']
 Route::resource('receive', ReceiveController::class)->middleware(['auth', 'verified']);
 Route::resource('send', SendController::class)->middleware(['auth', 'verified']);
 Route::resource('directsend', DirectSendController::class)->middleware(['auth', 'verified']);
+Route::resource('websocketconnections', WebsocketConnectionController::class)->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
