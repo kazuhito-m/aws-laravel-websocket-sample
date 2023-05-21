60218ec96dfc1178c8d0110115a7fa088a7d5acf
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-05-21 15:12:40 +0900

    implement: Factoryも使わなくなったし、Modelから余分なものを削除。

--- webapp/app/Models/Websocket/WebsocketConnection.php
+++ webapp/app/Models/Websocket/WebsocketConnection.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class WebsocketConnection extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'id',
+        'connection_id',
+        'user_id',
+        'created_at'
+    ];
 }
