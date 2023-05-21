43efaae6424283ff5a7b9438b53063f496ab17d4
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-05-21 16:00:04 +0900

    implement: ConnectionIDをDBから取得し、APIGatewayへ送信するように。

--- webapp/app/Http/Controllers/DirectSendController.php
+++ webapp/app/Http/Controllers/DirectSendController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\DirectSend\ClientPushSignalForWebSocketEndpoint;
+use App\Models\Websocket\WebsocketConnection;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
@@ -35,25 +36,30 @@ class DirectSendController extends Controller
     private function sendMessageOf(string $id, string $message)
     {
         Log::debug('ID:' . $id . ', message:' . $message);
+
+        $websocketConnections = WebsocketConnection::query()
+            ->where('user_id', intval($id))
+            ->get();
+        Log::debug('connection_ids:' . $websocketConnections);
+
         $signal = ClientPushSignalForWebSocketEndpoint::of($id, $message, Auth::user());
 
-        $this->sendDirectEndPointOfWebSocket($signal);
+        $this->sendDirectEndPointOfWebSocket($signal, $websocketConnections);
     }
 
-    private function sendDirectEndPointOfWebSocket(ClientPushSignalForWebSocketEndpoint $signal)
+    private function sendDirectEndPointOfWebSocket(ClientPushSignalForWebSocketEndpoint $signal, $websocketConnections)
     {
-        $endpoint = config('custom.websocket-url');
-        Log::debug('endpoint:' . $endpoint);  
-
         $client = new ApiGatewayManagementApiClient([
             'version' => '2018-11-29',
             'endpoint' => config('custom.websocket-url'),
             'region' => config('custom.websocket-api-region')
         ]);
 
-        $client->postToConnection([
-            'ConnectionId' => $signal->message, // TODO とても仮実装。DBに貯めて、そっから取り出す方法。
-            'Data' => json_encode($signal),
-        ]);
+        foreach ($websocketConnections as $websocketConnection) {
+            $client->postToConnection([
+                'ConnectionId' => $websocketConnection->connection_id,
+                'Data' => json_encode($signal),
+            ]);
+        }
     }
 }
