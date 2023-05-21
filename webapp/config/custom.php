6730df540a826481bc5f7ad7f42644ef6f3abd03
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-05-21 22:05:55 +0900

    implement: 過渡期のため、定数が統一できなそうなので、設定値を2つに分ける。

--- webapp/config/custom.php
+++ webapp/config/custom.php
@@ -1,6 +1,7 @@
 <?php
 return [
   'websocket-url' => env('WEBSOCKET_URL'),
+  'websocket-api-url' => env('WEBSOCKET_API_URL'),
   'websocket-api-region' => env('WEBSOCKET_API_REGION'),
   'client-send-api-url' => env('CLIENT_SEND_API_URL'),
 ];
