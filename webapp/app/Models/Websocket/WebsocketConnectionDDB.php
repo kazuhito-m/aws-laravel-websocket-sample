96362f98f7308425c9e1f703752a46cdba3e7307
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-05-22 08:36:02 +0900

    implement: DynamoDBのレコードを取ってこれる構造体を定義。

---
+++ webapp/app/Models/Websocket/WebsocketConnectionDDB.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models\Websocket;
+
+class WebsocketConnectionDDB
+{
+    public $connectionId;
+    public $userId;
+    public $connectedTime;
+
+
+    public static function of(string $connectionId, string $userId, string $connectedTime)
+    {
+        $con = new WebsocketConnectionDDB();
+
+        $con->connectionId = $connectionId;
+        $con->userId = $userId;
+        $con->connectedTime = $connectedTime;
+
+        return $signal;
+    }
+}
