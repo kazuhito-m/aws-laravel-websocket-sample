8231b29e40638f1dfa736e7b7006679d519103c7
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-05-21 14:08:21 +0900

    implement: スキャッフォルドでモデル、マイグレーション、コントローラ等一式を定義。

---
+++ webapp/app/Http/Controllers/WebsocketConnectionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreWebsocketConnectionRequest;
+use App\Http\Requests\UpdateWebsocketConnectionRequest;
+use App\Models\Websocket\WebsocketConnection;
+
+class WebsocketConnectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreWebsocketConnectionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(WebsocketConnection $websocketConnection)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(WebsocketConnection $websocketConnection)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateWebsocketConnectionRequest $request, WebsocketConnection $websocketConnection)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(WebsocketConnection $websocketConnection)
+    {
+        //
+    }
+}
---
+++ webapp/app/Models/Websocket/WebsocketConnection.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Websocket;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WebsocketConnection extends Model
+{
+    use HasFactory;
+}
