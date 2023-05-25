07833521064478aae110ca858246a08db174fc15
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-05-26 05:23:08 +0900

    implement: RDS部分も実装。

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as rds from "aws-cdk-lib/aws-rds";
 import { readFileSync } from 'fs';
 
 export class InfrastructureStack extends Stack {
@@ -27,7 +28,21 @@ export class InfrastructureStack extends Stack {
     const script = readFileSync("./lib/resources/user-data.sh", "utf8");
     webServer1.addUserData(script);
 
-     // port80, 全ての IP アドレスからのアクセスを許可
-     webServer1.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
+    // port80, 全ての IP アドレスからのアクセスを許可
+    webServer1.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
+
+    // RDS
+    const dbServer = new rds.DatabaseInstance(this, "WordPressDB", {
+      vpc,
+      // DatabaseInstanceEngine クラスを利用してデータベースエンジンを設定
+      engine: rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_8_0_31 }),
+      // RDS DB インスタンスのインスタンスタイプを設定
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.SMALL),
+      // RDS DB インスタンスのデータベース名を設定
+      databaseName: "wordpress",
+    });
+
+    // WebServer からのアクセスを許可
+    dbServer.connections.allowDefaultPortFrom(webServer1);
   }
 }
