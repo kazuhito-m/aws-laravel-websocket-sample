f28729aaa9d63820e1e688612cbe3eb7c1d12faf
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-05-26 06:59:45 +0900

    implement: ALB構成に付く変える。

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -5,7 +5,6 @@ import * as rds from "aws-cdk-lib/aws-rds";
 import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
 import * as targets from "aws-cdk-lib/aws-elasticloadbalancingv2-targets";
 
-// 自作コンストラクトを import
 import { WebServerInstance } from './constructs/web-server-instance';
 
 export class InfrastructureStack extends Stack {
@@ -16,8 +15,10 @@ export class InfrastructureStack extends Stack {
             ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
         });
 
-        // 新しく作成したコンストラクトを使用してインスタンスを宣言
-        const webServer1 = new WebServerInstance(this, 'WebServer1', { vpc });
+        const webServer1 = new WebServerInstance(this, 'WebServer1', { vpc, });
+
+        // 2 台目のインスタンスを宣言
+        const webServer2 = new WebServerInstance(this, 'WebServer2', { vpc, });
 
         const dbServer = new rds.DatabaseInstance(this, "WordPressDB", {
             vpc,
@@ -26,8 +27,9 @@ export class InfrastructureStack extends Stack {
             databaseName: "wordpress",
         });
 
-        // アクセス許可対象をコンストラクト内のインスタンスに変更
         dbServer.connections.allowDefaultPortFrom(webServer1.instance);
+        // 2 台目のインスタンスの DB インスタンスへのアクセスを許可
+        dbServer.connections.allowDefaultPortFrom(webServer2.instance);
 
         const alb = new elbv2.ApplicationLoadBalancer(this, "LoadBalancer", {
             vpc,
@@ -38,14 +40,16 @@ export class InfrastructureStack extends Stack {
         });
         listener.addTargets("ApplicationFleet", {
             port: 80,
-            // ターゲットをコンストラクト内のインスタンスに変更
-            targets: [new targets.InstanceTarget(webServer1.instance, 80)],
+            targets: [new targets.InstanceTarget(webServer1.instance, 80),
+            // ターゲットに 2 台目のインスタンスを追加
+            new targets.InstanceTarget(webServer2.instance, 80)],
             healthCheck: {
                 path: "/wp-includes/images/blank.gif",
             },
         });
 
-        // ALB からインスタンスへのアクセスを許可
         webServer1.instance.connections.allowFrom(alb, ec2.Port.tcp(80));
+        // ALB から 2 台目のインスタンスへのアクセスを許可
+        webServer2.instance.connections.allowFrom(alb, ec2.Port.tcp(80));
     }
 }
