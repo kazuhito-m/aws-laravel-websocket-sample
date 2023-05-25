e443e059c33935f6597c9f698cf7711b3832e29c
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-05-26 05:00:23 +0900

    implement: ec2インスタンスの設定を追加。

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,10 +1,33 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { readFileSync } from 'fs';
+
 export class InfrastructureStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // TODO 実装。
+    // VPC
+    const vpc = new ec2.Vpc(this, "BlogVpc", {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+    });
+
+
+    // EC2
+    const webServer1 = new ec2.Instance(this, "WordpressServer1", {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.SMALL),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+    });
+
+    const script = readFileSync("./lib/resources/user-data.sh", "utf8");
+    webServer1.addUserData(script);
+
+     // port80, 全ての IP アドレスからのアクセスを許可
+     webServer1.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
   }
 }
