248cdaef9c9392549a1606860bfd7df637bd3ee7
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-05-28 00:24:07 +0900

    refactor: importの順番をいい感じに。

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,11 +1,11 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib/core";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as rds from "aws-cdk-lib/aws-rds";
 import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
 import * as targets from "aws-cdk-lib/aws-elasticloadbalancingv2-targets";
 import * as route53 from "aws-cdk-lib/aws-route53";
-import * as cdk from "aws-cdk-lib/core";
 
 import { WebServerInstance } from './constructs/web-server-instance';
 
