c10b96e83e1a44f1b24e0996a1bccdc142ba3303
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-05-29 06:14:47 +0900

    implement: 削除設定を見直し。

--- aws-infra/global/lib/alws-global-stack.ts
+++ aws-infra/global/lib/alws-global-stack.ts
@@ -1,6 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
-import { RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { AlwsGlobalStackProps } from './alws-global-stack-props';
 
@@ -13,9 +12,9 @@ export class AlwsGlobalStack extends cdk.Stack {
         const containerRepository = new ecr.Repository(this, 'ContainerRepsitory', {
             repositoryName: settings?.containerImageId(),
             imageTagMutability: ecr.TagMutability.IMMUTABLE,
-            removalPolicy: RemovalPolicy.DESTROY,
-            autoDeleteImages: false,  // Stack削除時、連鎖削除設定だが、イメージが一つでも在れば削除しない。            
+            imageScanOnPush: true,
         });
+        // Stack削除時、連鎖削除設定だが、イメージが一つでも在れば削除せず、Stackから外れる。
         containerRepository.addLifecycleRule({
             maxImageCount: 500
         })
