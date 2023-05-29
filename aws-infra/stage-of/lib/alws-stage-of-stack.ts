458229b6e3d2a2c2d6de3f62b94622281c1f28a3
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-05-29 23:11:57 +0900

    fix format.

--- aws-infra/stage-of/lib/alws-stage-of-stack.ts
+++ aws-infra/stage-of/lib/alws-stage-of-stack.ts
@@ -3,14 +3,7 @@ import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AlwsStageOfStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AlwsStageOfQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+    }
 }
