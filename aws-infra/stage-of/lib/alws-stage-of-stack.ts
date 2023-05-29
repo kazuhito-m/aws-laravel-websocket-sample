eb6700da51296bbb1acba42ec03b5185cc04fc57
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-05-30 01:35:03 +0900

    refactor: 邪魔な一文はまとめて後ろに持ってく。

--- aws-infra/stage-of/lib/alws-stage-of-stack.ts
+++ aws-infra/stage-of/lib/alws-stage-of-stack.ts
@@ -1,17 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { AlwsStackProps } from './alws-stack-props';
+import { Context } from './context/context';
 
 export class AlwsStageOfStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: AlwsStackProps) {
         super(scope, id, props);
 
-        const settings = props?.context;
+        this.confimationOfPreconditions(props?.context);
+
+
+    }
+
+    private confimationOfPreconditions(settings?: Context): void {
         if (!settings) throw new Error('cdk.json の内容が読めませんでした。');
 
         if (settings.invalidCurrentStageId())
             throw new Error(`stageIdが正しく指定されていません。(${settings.currentStageId}) 有効なstageIdは ${settings.stageIdsText()} のいずれかです。`);
-
-        console.log(settings.currentStageIdOfPascalCase());
     }
 }
