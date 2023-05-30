876639436f472b15e51d0654f4bc490fa323e0d2
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-05-30 16:29:59 +0900

    refactor: 意味別でパラメータの順序を変え、まとめてみる。

--- aws-infra/stage-of/lib/alws-stage-of-stack.ts
+++ aws-infra/stage-of/lib/alws-stage-of-stack.ts
@@ -1,9 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as rds from "aws-cdk-lib/aws-rds";
+import * as sm from "aws-cdk-lib/aws-secretsmanager";
 import { SubnetType } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 import { AlwsStackProps } from './alws-stack-props';
 import { Context } from './context/context';
+import { SecretValue } from 'aws-cdk-lib';
 
 export class AlwsStageOfStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: AlwsStackProps) {
@@ -32,10 +35,60 @@ export class AlwsStageOfStack extends cdk.Stack {
                 }
             ]
         });
-        const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroupEcs', {
+        const rdsSecurityGroup = new ec2.SecurityGroup(this, 'SecurityGroupEcs', {
             vpc: vpc,
-            securityGroupName: 'sg-ecs'
+            securityGroupName: settings.wpk('rds-sg')
         });
+
+        // RDS
+        const rdsSecret = new sm.Secret(this, settings.wpp("RdsAppSecret"), {
+            secretName: settings.wpk("rds-app-secret"),
+            generateSecretString: {
+                excludePunctuation: true,
+                includeSpace: false,
+                secretStringTemplate: JSON.stringify({ username: 'user' }),
+                generateStringKey: 'password',
+            },
+        });
+        const rdsCredential = rds.Credentials.fromPassword(
+            rdsSecret.secretValueFromJson('username').toString(),
+            SecretValue.unsafePlainText(
+                rdsSecret.secretValueFromJson('password').toString()
+            )
+        )
+
+        const rdsSettings = settings.currentStage().rds;
+
+        const appDb = new rds.DatabaseInstance(this, settings.wpp("App"), {
+            engine: rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_8_0_32 }),
+            instanceType: ec2.InstanceType.of(
+                rdsSettings.class,
+                rdsSettings.size
+            ),
+            multiAz: rdsSettings.multiAz,
+            databaseName: settings.systemName(),
+            vpc,
+            vpcSubnets: vpc.selectSubnets(),
+            securityGroups: [rdsSecurityGroup],
+            credentials: rdsCredential,
+        });
+
+
+
+        // TODO 下を参考に、情報をコンテナ側へ環境変数で渡す
+        // const container = taskDefinition.addContainer('Container', {
+        //     // image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+        //     image: ecs.ContainerImage.fromEcrRepository(repository),
+        //     memoryLimitMiB: 256,
+        //     cpu: 256,
+        //     environment: {
+        //         DB_HOST: postgresql.instanceEndpoint.hostname,
+        //         DB_PORT: String(postgresql.instanceEndpoint.port),
+        //         DB_NAME: 'testdatabase',
+        //         DB_USER: databaseCredentialSecret.secretValueFromJson('username').toString(),
+        //         APP_DATABASE_PASSWORD: databaseCredentialSecret.secretValueFromJson('password').toString(),
+        //     },
+        // })
 
         this.setTag("Stage", settings.currentStageId);
         this.setTag("Version", settings.packageVersion());
