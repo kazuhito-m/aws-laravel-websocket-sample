03b57022e9d51e423c44da5b5112a8e490c96cac
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-03 02:45:07 +0900

    implement: lambdaの実装をTypeScript版に置き換えていく。

--- aws-lambda/websocket/index.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event, context) => {
-  console.log('これ、もしかして正しく出ている？');
-  console.log(event);
-  console.log(context);
-
-  return { statusCode: 200, body: '途中で終わった系の関数。' };
-};
