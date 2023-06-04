ccca49c9715523c325dd7f2ed45e88003d44b1a6
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-05 02:51:58 +0900

    refactor: メソッド分割で見通し良く。

--- aws-lambda/websocket/src/websocket-inner-route.ts
+++ aws-lambda/websocket/src/websocket-inner-route.ts
@@ -3,9 +3,13 @@ import { DelegeteLambda } from './delegete-lambda'
 
 export class WebSocketInnterRoute implements DelegeteLambda {
     public async handler(event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> {
+        return this.res(200, 'Send WebSocket successed.');
+    }
+
+    protected res(status: number, message: string): APIGatewayProxyResult {
         return {
-            statusCode: 200,
-            body: JSON.stringify({ message: 'Send WebSocket successed.' }),
+            statusCode: status,
+            body: JSON.stringify({ message: message })
         };
     }
 }
