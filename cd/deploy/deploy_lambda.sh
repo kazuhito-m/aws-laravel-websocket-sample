612367e3023c2755b99b88c58b56cc9466b2e91a
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-04 19:43:03 +0900

    implement: とりあえず、Lambdaのデプロイ実装を仮組。

--- cd/deploy/deploy_lambda.sh
+++ cd/deploy/deploy_lambda.sh
@@ -14,19 +14,6 @@ set -eux
 VERSION_TAG=${1}
 CONTAINER_REGISTRY_TAG_URI=${CONTAINER_REGISTRY_URI_LAMBDA}:${VERSION_TAG}
 
-lambda_function_name_01=${LAMBDA_FUNCTION_NAMES/,*/}
-
-# aws ecs describe-task-definition --task-definition ${ECS_TASK_FAMILY} | \
-#     jq '.taskDefinition | del (.taskDefinitionArn, .revision, .status, .requiresAttributes, .compatibilities, .registeredAt, .registeredBy)' \
-#     > ./taskdef.json
-
-# sed -Ei "s/\"image\": (.*):(.*)\",/\"image\": \1:${VERSION_TAG}\",/g" ./taskdef.json
-
-# TDOO ECSにコンテナがあるかどうかをチェックして、なかったら殺す。
-
-# aws ecs register-task-definition --cli-input-json file://taskdef.json > ./register-result.json
-# aws ecs update-service --cluster ${ECS_CLUSTER} --service ${ECS_SERVICE} --task-definition ${ECS_TASK_FAMILY}
-
 for function_name in ${LAMBDA_FUNCTION_NAMES/,/ }; do
     echo aws lambda update-function-code --region ${AWS_DEFAULT_REGION} \
         --image-uri ${CONTAINER_REGISTRY_TAG_URI} \
