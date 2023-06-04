60af88c2e4504361b4bfcc46143c9516a777e7bf
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-05 01:55:51 +0900

    implement: さらなるもう1パターンのルートの実装。

--- aws-lambda/websocket/src/index.ts
+++ aws-lambda/websocket/src/index.ts
@@ -2,14 +2,20 @@ import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
 import { OnConnectWebSocket } from './on-connected-websocket';
 import { OnDisconnectedWebSocket } from './on-disconnected-websocket';
 import { DelegeteLambda } from './delegete-lambda';
+import { WebSocketInnterRoute } from './websocket-inner-route';
 
 const handlers: { [key: string]: DelegeteLambda; } = {
     'CONNECT': new OnConnectWebSocket(),
     'DISCONNECT': new OnDisconnectedWebSocket(),
+    'INNER_ROUTE': new WebSocketInnterRoute()
 };
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
-    const handle = handlers[ event.requestContext.eventType as string];
+    const handlerId = context.functionName.includes('send-websocket-inner-route-lambda')
+        ? 'INNER_ROUTE'
+        : event.requestContext.eventType as string;
+    const handle = handlers[handlerId];
     if (!handle) return { statusCode: 400, body: 'Handler not found.' };
+
     return await handle.handler(event, context);
 }
