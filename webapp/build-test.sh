2f8914095e93c88a86a15ad43eee2171238eaf40
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-09 01:13:46 +0900

    implement: ビルド＆Runした後、バージョンファイルを元に戻す仕組みを入れる。

--- webapp/build-test.sh
+++ webapp/build-test.sh
@@ -4,12 +4,12 @@ set -e
 
 ./container/rewrite_version.sh '0.0.9999'
 
-exit 0
-
 # docker build -t laravel-test:latest . \
 docker build --no-cache -t laravel-test:latest .
 docker run -p 80:80 --name la-test --env TEST_VERBOSE='docker runの環境変数に書くだけで上書き出来た。' laravel-test:latest
- 
+
+git checkout ./version.php
+
 docker rm -f la-test
 docker rmi laravel-test:latest
 
