f91493debb044ca43441e1b0166832a9f539d3ee
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-09 01:14:18 +0900

    refactor: ファイルを元に戻すタイミングを変える。

--- webapp/build-test.sh
+++ webapp/build-test.sh
@@ -6,9 +6,10 @@ set -e
 
 # docker build -t laravel-test:latest . \
 docker build --no-cache -t laravel-test:latest .
-docker run -p 80:80 --name la-test --env TEST_VERBOSE='docker runの環境変数に書くだけで上書き出来た。' laravel-test:latest
 
 git checkout ./version.php
+
+docker run -p 80:80 --name la-test --env TEST_VERBOSE='docker runの環境変数に書くだけで上書き出来た。' laravel-test:latest
 
 docker rm -f la-test
 docker rmi laravel-test:latest
