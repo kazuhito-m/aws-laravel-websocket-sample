f8aa002540946a59c0a39bd610633e84d55fda70
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-09 23:06:27 +0900

    refactor: buildEcsClusterの箇所を、Constructに。

--- aws-infra/stage-of/lib/alws-stage-of-stack.ts
+++ aws-infra/stage-of/lib/alws-stage-of-stack.ts
@@ -1,24 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
-import * as ec2 from "aws-cdk-lib/aws-ec2";
-import * as rds from "aws-cdk-lib/aws-rds";
-import * as ecs from "aws-cdk-lib/aws-ecs";
-import * as ecsPatterns from "aws-cdk-lib/aws-ecs-patterns";
-import * as sm from "aws-cdk-lib/aws-secretsmanager";
-import * as elb from 'aws-cdk-lib/aws-elasticloadbalancingv2';
-import * as logs from 'aws-cdk-lib/aws-logs';
-import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 import { AlwsStackProps } from './alws-stack-props';
 import { Context } from './context/context';
-import { Duration } from 'aws-cdk-lib';
-import { HostedZone, ARecord, RecordTarget } from 'aws-cdk-lib/aws-route53';
-import { LoadBalancerTarget } from 'aws-cdk-lib/aws-route53-targets';
-import { StringParameter } from 'aws-cdk-lib/aws-ssm';
-import { RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { VpcAndNetwork } from './construct/vpc-and-network';
 import { ApplicationRds } from './construct/application-rds';
 import { ApiGatewayAndLambda } from './construct/apigateway-and-lambda';
 import { CodeBuildForCdDeploy } from './construct/code-build-for-cd-deploy';
+import { EcsCluster } from './construct/ecs-cluster';
 
 export class AlwsStageOfStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: AlwsStackProps) {
@@ -33,139 +21,19 @@ export class AlwsStageOfStack extends cdk.Stack {
 
         const apiAndLambda = new ApiGatewayAndLambda(this, 'ApiGatewayAndLambda', { context: context });
 
-        this.buildEcsCluster(context, vpc.vpc, rds.appRds, vpc.ecsSecurityGroup, rds.rdsSecret, apiAndLambda.innerApi);
+        new EcsCluster(this, 'EcsCluster', {
+            context: context,
+            vpc: vpc.vpc,
+            rds: rds.appRds,
+            rdsSecret: rds.rdsSecret,
+            ecsSecurityGroup: vpc.ecsSecurityGroup,
+            innerApi: apiAndLambda.innerApi
+        });
 
         new CodeBuildForCdDeploy(this, 'CodeBuildForCdDeploy', { context: context })
 
         this.setTag("Stage", context.currentStageId);
         this.setTag("Version", context.packageVersion());
-    }
-
-    private buildEcsCluster(settings: Context,
-        vpc: ec2.Vpc,
-        rds: rds.DatabaseInstance,
-        ecsSecurityGroup: ec2.SecurityGroup,
-        rdsSecret: sm.Secret,
-        innerApi: RestApi
-    ) {
-        const ecsCluster = new ecs.Cluster(this, settings.wpp("EcsCluster"), {
-            clusterName: settings.wpk('ecs-cluster'),
-            vpc: vpc,
-        });
-
-        const serviceTaskDefinition = new ecs.FargateTaskDefinition(this, `${settings.systemNameOfPascalCase()}AppTaskDefinition`, {
-            family: settings.wpk('app-task-difinition-family'),
-            cpu: 256,
-            memoryLimitMiB: 512,
-            runtimePlatform: {
-                cpuArchitecture: ecs.CpuArchitecture.X86_64,
-                operatingSystemFamily: ecs.OperatingSystemFamily.LINUX
-            },
-            // TODO 実行時ロールの作り込み
-            executionRole: new iam.Role(this, 'TaskExecutionRole', {
-                assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
-                inlinePolicies: {
-                    "ApiGatewayManagementForWebSocketRequestPolicy": iam.PolicyDocument.fromJson({
-                        "Version": "2012-10-17",
-                        "Statement": [
-                            {
-                                "Effect": "Allow",
-                                "Action": "execute-api:ManageConnections",
-                                "Resource": "arn:aws:execute-api:*:*:*/*/*/*"
-                            }
-                        ]
-                    })  // FIXME これはレンジ広すぎてひどい…
-                }
-            })
-        });
-        const containerName = `${settings.systemName()}-app`;
-        serviceTaskDefinition.addContainer(`${settings.systemNameOfPascalCase()}AppContainer`, {
-            containerName: containerName,
-            image: ecs.ContainerImage.fromRegistry("nginx:mainline-alpine"),
-            memoryReservationMiB: 256,
-            logging: ecs.LogDriver.awsLogs({
-                streamPrefix: 'ecs',
-                logGroup: new logs.LogGroup(this, 'ContainerLogGroup', {
-                    logGroupName: `/ecs/${settings.currentStageId}/${containerName}`,
-                    retention: 30,
-                }),
-            }),
-            healthCheck: {
-                command: ["CMD-SHELL", "curl -f http://localhost/ || exit 1"],
-                interval: Duration.seconds(30),
-                timeout: Duration.seconds(5),
-                retries: 3
-            },
-            environment: {
-                DB_HOST: rds.instanceEndpoint.hostname,
-                DB_PORT: String(rds.instanceEndpoint.port),
-                DB_DATABASE: settings.systemName(),
-                DB_USERNAME: rdsSecret.secretValueFromJson('username').unsafeUnwrap(),
-                DB_PASSWORD: rdsSecret.secretValueFromJson('password').unsafeUnwrap(),
-                CLIENT_SEND_API_URL: innerApi.url,
-                WEBSOCKET_URL: settings.currentStage().apiFqdn,
-                WEBSOCKET_API_URL: settings.websocketEndpointUrl(),
-                WEBSOCKET_API_REGION: this.region,
-                // TODO 以下は「何をどうやって仕込むか」を要検討
-                // WSDDB_AWS_ACCESS_KEY_ID: '',
-                // WSDDB_AWS_SECRET_ACCESS_KEY: ''
-            }
-        }).addPortMappings({
-            name: `${settings.systemName()}-app-80-tcp`,
-            containerPort: 80,
-            hostPort: 80,
-            protocol: ecs.Protocol.TCP,
-            appProtocol: ecs.AppProtocol.http
-        });
-
-        const albFargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'AppService', {
-            serviceName: settings.wpk('app-service'),
-            taskDefinition: serviceTaskDefinition,
-            securityGroups: [ecsSecurityGroup],
-            healthCheckGracePeriod: Duration.seconds(240),
-            loadBalancerName: settings.wpk('app-alb'),
-            // redirectHTTP: true,  // TODO この設定だけでは80塞ぎが出来ないよう、後で
-            cluster: ecsCluster,
-        });
-        albFargateService.targetGroup.configureHealthCheck({
-            path: "/",
-            healthyThresholdCount: 2,
-            interval: Duration.seconds(15),
-        });
-        if (settings.isContainerAutoScaling()) {
-            const config = settings.currentStage().container;
-            const autoScaling = albFargateService.service.autoScaleTaskCount({
-                minCapacity: config.minCapacity,
-                maxCapacity: config.maxCapacity,
-            });
-            autoScaling.scaleOnCpuUtilization('CpuBurstControl', {
-                targetUtilizationPercent: config.cpuUtilizationPercent
-            })
-        }
-
-        const certificateArn = StringParameter.valueFromLookup(this, settings.certArnPraStoreName());
-        const certificate = elb.ListenerCertificate.fromArn(certificateArn);
-
-        albFargateService.loadBalancer.addListener('AlbListenerHTTPS', {
-            protocol: elb.ApplicationProtocol.HTTPS,
-            defaultAction: elb.ListenerAction.forward([albFargateService.targetGroup]),
-            sslPolicy: elb.SslPolicy.RECOMMENDED_TLS,
-            certificates: [certificate]
-        });
-
-        const hostedZoneId = StringParameter.valueFromLookup(this, settings.hostedZoneIdPraStoreName());
-        const hostedZone = HostedZone.fromHostedZoneAttributes(this, "HostZone", {
-            zoneName: settings.applicationDnsARecordName(),
-            hostedZoneId: hostedZoneId,
-        });
-        new ARecord(this, "DnsAppAnameRecord", {
-            zone: hostedZone,
-            recordName: settings.applicationDnsARecordName(),
-            target: RecordTarget.fromAlias(new LoadBalancerTarget(albFargateService.loadBalancer)),
-            ttl: Duration.minutes(5),
-            comment: 'Application LB Record.'
-        });
-        return ecsCluster;
     }
 
     private setTag(key: string, value: string): void {
--- aws-infra/stage-of/lib/construct/ecs-cluster.ts
+++ aws-infra/stage-of/lib/construct/ecs-cluster.ts
@@ -1,18 +1,152 @@
 import { SecurityGroup, Vpc } from 'aws-cdk-lib/aws-ec2'
 import { Construct } from 'constructs';
 import { Context } from '../context/context';
+import { RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
+import { DatabaseInstance } from 'aws-cdk-lib/aws-rds';
+import { AppProtocol, Cluster, ContainerImage, CpuArchitecture, FargateTaskDefinition, LogDriver, OperatingSystemFamily, Protocol } from 'aws-cdk-lib/aws-ecs';
+import { PolicyDocument, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { LogGroup } from 'aws-cdk-lib/aws-logs';
+import { Duration, Stack } from 'aws-cdk-lib';
+import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
+import { StringParameter } from 'aws-cdk-lib/aws-ssm';
+import { ApplicationProtocol, ListenerAction, ListenerCertificate, SslPolicy } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { ARecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
+import { LoadBalancerTarget } from 'aws-cdk-lib/aws-route53-targets';
 
 export interface EcsClusterProps {
-    readonly vpc: Vpc;
-    readonly rdsSecurityGroup: SecurityGroup;
-    readonly ecsSecurityGroup: SecurityGroup;
     readonly context: Context;
+    readonly vpc: Vpc;
+    readonly ecsSecurityGroup: SecurityGroup;
+    readonly rds: DatabaseInstance;
+    readonly rdsSecret: Secret;
+    readonly innerApi: RestApi;
 }
 
 export class EcsCluster extends Construct {
     constructor(scope: Construct, id: string, props: EcsClusterProps) {
         super(scope, id);
 
+        const stack = scope as Stack;
+        const context = props.context;
 
+        const ecsCluster = new Cluster(this, context.wpp("EcsCluster"), {
+            clusterName: context.wpk('ecs-cluster'),
+            vpc: props.vpc,
+        });
+
+        const serviceTaskDefinition = new FargateTaskDefinition(this, `${context.systemNameOfPascalCase()}AppTaskDefinition`, {
+            family: context.wpk('app-task-difinition-family'),
+            cpu: 256,
+            memoryLimitMiB: 512,
+            runtimePlatform: {
+                cpuArchitecture: CpuArchitecture.X86_64,
+                operatingSystemFamily: OperatingSystemFamily.LINUX
+            },
+            // TODO 実行時ロールの作り込み
+            executionRole: new Role(this, 'TaskExecutionRole', {
+                assumedBy: new ServicePrincipal('ecs-tasks.amazonaws.com'),
+                inlinePolicies: {
+                    "ApiGatewayManagementForWebSocketRequestPolicy": PolicyDocument.fromJson({
+                        "Version": "2012-10-17",
+                        "Statement": [
+                            {
+                                "Effect": "Allow",
+                                "Action": "execute-api:ManageConnections",
+                                "Resource": "arn:aws:execute-api:*:*:*/*/*/*"
+                            }
+                        ]
+                    })  // FIXME これはレンジ広すぎてひどい…
+                }
+            })
+        });
+        const containerName = `${context.systemName()}-app`;
+        serviceTaskDefinition.addContainer(`${context.systemNameOfPascalCase()}AppContainer`, {
+            containerName: containerName,
+            image: ContainerImage.fromRegistry("nginx:mainline-alpine"),
+            memoryReservationMiB: 256,
+            logging: LogDriver.awsLogs({
+                streamPrefix: 'ecs',
+                logGroup: new LogGroup(this, 'ContainerLogGroup', {
+                    logGroupName: `/ecs/${context.currentStageId}/${containerName}`,
+                    retention: 30,
+                }),
+            }),
+            healthCheck: {
+                command: ["CMD-SHELL", "curl -f http://localhost/ || exit 1"],
+                interval: Duration.seconds(30),
+                timeout: Duration.seconds(5),
+                retries: 3
+            },
+            environment: {
+                DB_HOST: props.rds.instanceEndpoint.hostname,
+                DB_PORT: String(props.rds.instanceEndpoint.port),
+                DB_DATABASE: context.systemName(),
+                DB_USERNAME: props.rdsSecret.secretValueFromJson('username').unsafeUnwrap(),
+                DB_PASSWORD: props.rdsSecret.secretValueFromJson('password').unsafeUnwrap(),
+                CLIENT_SEND_API_URL: props.innerApi.url,
+                WEBSOCKET_URL: context.currentStage().apiFqdn,
+                WEBSOCKET_API_URL: context.websocketEndpointUrl(),
+                WEBSOCKET_API_REGION: stack.region,
+                // TODO 以下は「何をどうやって仕込むか」を要検討
+                // WSDDB_AWS_ACCESS_KEY_ID: '',
+                // WSDDB_AWS_SECRET_ACCESS_KEY: ''
+            }
+        }).addPortMappings({
+            name: `${context.systemName()}-app-80-tcp`,
+            containerPort: 80,
+            hostPort: 80,
+            protocol: Protocol.TCP,
+            appProtocol: AppProtocol.http
+        });
+
+        const albFargateService = new ApplicationLoadBalancedFargateService(this, 'AppService', {
+            serviceName: context.wpk('app-service'),
+            taskDefinition: serviceTaskDefinition,
+            securityGroups: [props.ecsSecurityGroup],
+            healthCheckGracePeriod: Duration.seconds(240),
+            loadBalancerName: context.wpk('app-alb'),
+            // redirectHTTP: true,  // TODO この設定だけでは80塞ぎが出来ないよう、後で
+            cluster: ecsCluster,
+        });
+        albFargateService.targetGroup.configureHealthCheck({
+            path: "/",
+            healthyThresholdCount: 2,
+            interval: Duration.seconds(15),
+        });
+        if (context.isContainerAutoScaling()) {
+            const config = context.currentStage().container;
+            const autoScaling = albFargateService.service.autoScaleTaskCount({
+                minCapacity: config.minCapacity,
+                maxCapacity: config.maxCapacity,
+            });
+            autoScaling.scaleOnCpuUtilization('CpuBurstControl', {
+                targetUtilizationPercent: config.cpuUtilizationPercent
+            })
+        }
+
+        const certificateArn = StringParameter.valueFromLookup(this, context.certArnPraStoreName());
+        const certificate = ListenerCertificate.fromArn(certificateArn);
+
+        albFargateService.loadBalancer.addListener('AlbListenerHTTPS', {
+            protocol: ApplicationProtocol.HTTPS,
+            defaultAction: ListenerAction.forward([albFargateService.targetGroup]),
+            sslPolicy: SslPolicy.RECOMMENDED_TLS,
+            certificates: [certificate]
+        });
+
+        const hostedZoneId = StringParameter.valueFromLookup(this, context.hostedZoneIdPraStoreName());
+        const hostedZone = HostedZone.fromHostedZoneAttributes(this, "HostZone", {
+            zoneName: context.applicationDnsARecordName(),
+            hostedZoneId: hostedZoneId,
+        });
+        new ARecord(this, "DnsAppAnameRecord", {
+            zone: hostedZone,
+            recordName: context.applicationDnsARecordName(),
+            target: RecordTarget.fromAlias(new LoadBalancerTarget(albFargateService.loadBalancer)),
+            ttl: Duration.minutes(5),
+            comment: 'Application LB Record.'
+        });
+        return ecsCluster;
     }
 }
