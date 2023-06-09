af7638f5309f33ec57ad4610d805252798f43a57
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-09 11:17:54 +0900

    implement: テスト用のDockerRun実行用スクリプトに、環境変数を付与する。

--- webapp/build-test.sh
+++ webapp/build-test.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-set -e
-
 ./container/rewrite_version.sh '0.0.9999'
 
+echo '-------- docker build --------'
 # docker build -t laravel-test:latest . \
 docker build --no-cache -t laravel-test:latest .
 
 git checkout ./version.php
 
-docker run -p 80:80 --name la-test --env TEST_VERBOSE='docker runの環境変数に書くだけで上書き出来た。' laravel-test:latest
+echo '-------- docker run --------'
+docker run -p 80:80 --name la-test \
+    --env DB_CONNECTION='mysql' \
+    --env DB_HOST='192.168.1.133' \
+    --env DB_PORT='3306' \
+    --env DB_DATABASE='laravel' \
+    --env DB_USERNAME='root' \
+    --env DB_PASSWORD='local-password' \
+    --env TEST_VERBOSE='docker_runの環境変数に書くだけで上書き出来た。' \
+    laravel-test:latest
+
+#     --env TEST_VERBOSE='docker_runの環境変数に書くだけで上書き出来た。' \
 
 docker rm -f la-test
 docker rmi laravel-test:latest
-
