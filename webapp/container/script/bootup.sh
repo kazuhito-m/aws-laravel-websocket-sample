552abca53411566ee57fa5745f25294916c1ad02
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-09 14:25:12 +0900

    implement: 起動スクリプトの「bashのコメント」にしていたものは、いっそのこと「出力する文言」に変更してしまう。
    このプロジェクト自体がお試しなので、そっちのほうがデバッグしやすいので。

--- webapp/container/script/bootup.sh
+++ webapp/container/script/bootup.sh
@@ -7,16 +7,11 @@ set -eu
 SCRIPT_DIR=$(cd $(dirname $(readlink -f $0 || echo $0));pwd -P)
 cd ${SCRIPT_DIR}
 
-echo '---- 設定ファイルを環境変数で上書き。----'
 overwrite_env.sh '/app/.env'
 
 echo '---- Laravelのマイグレーションを起動時に必ず行う。 ----'
-
-# Laravelのマイグレーションを起動時に必ず行う。
 /app/migration.sh
 
-echo '---- nginx始め、各種サービス立ち上げ。 ----'
-
-# nginx始め、各種サービス立ち上げ。
+echo '---- nginx, php-fpm, 各種サービス立ち上げ。 ----'
 supervisord
 
