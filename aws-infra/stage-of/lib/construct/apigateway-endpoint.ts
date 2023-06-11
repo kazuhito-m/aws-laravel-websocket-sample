23c5c28b5363d2c9c868137e26d724b68c9707ee
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-11 20:10:37 +0900

    bugfix: Lambda側が統一感取れてなかったので、環境変数の内容を統一。

--- aws-infra/stage-of/lib/construct/apigateway-endpoint.ts
+++ aws-infra/stage-of/lib/construct/apigateway-endpoint.ts
@@ -1,6 +1,8 @@
 import { CfnStage } from "aws-cdk-lib/aws-apigatewayv2";
 
 export class ApiGatewayEndpoint {
+    public static readonly STAGE_NAME = 'v1';
+
     constructor(private readonly apiStage: CfnStage) { }
 
     public path(): string {
