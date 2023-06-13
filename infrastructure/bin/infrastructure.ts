b49061381b805742e65f96b3004bbe8814ca9c98
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-13 15:14:07 +0900

    implement: CDKの2つの機能の実験をする。
    - パラメータストアの読み書きの整合性(失敗)
    - CDK Stackの複数化

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { InfrastructureStack } from '../lib/infrastructure-stack';
+import { Test2Stack } from '../lib/test2-stack';
+import { TestStack } from '../lib/test-stack';
 
 const app = new cdk.App();
-new InfrastructureStack(app, 'InfrastructureStack');
+
+const props = {
+    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
+};
+
+new InfrastructureStack(app, 'InfrastructureStack', props);
+new TestStack(app, 'TestStack', props);
+new Testack(app, 'Test2Stack', props);
---
+++ infrastructure/lib/test-stack.ts
@@ -0,0 +1,16 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { StringParameter } from 'aws-cdk-lib/aws-ssm';
+import { Construct } from 'constructs';
+
+export class TestStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+        // FIXME 設定値は、第2引数のIDではない「よくわからない謎のID」になる…。
+        new StringParameter(this, 'test-parameter', { stringValue: 'これがでればOK' });
+        new StringParameter(this, 'MiuraKazuhito', { stringValue: '三浦一仁' });
+        new StringParameter(this, '日本語パラメータ', { stringValue: '文字列が表示されるかな。' });
+
+        console.log('これが表示されたら、動いたってことで');
+    }
+}
