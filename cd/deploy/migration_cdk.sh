7545bfd6e2ab2f77a28928a9ab9152199169e0fc
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-15 16:58:53 +0900

    implement: 「実行できる土台」まで作る。

---
+++ cd/deploy/migration_cdk.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#
+# 前提として必要なコマンド
+#   jq,
+#
+# 前提として必要な環境変数
+#   STAGE_ID, CONTAINER_REGISTRY_URI_APP, ECS_CLUSTER, ECS_SERVICE, ECS_TASK_FAMILY
+#
+
+set -eux
+
+VERSION_TAG=${1}
+
+exit 0
