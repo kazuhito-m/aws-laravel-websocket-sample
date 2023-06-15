78693de06175e51e23301197989eb9a0195bc438
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-16 03:27:09 +0900

    bugfix: 足りない権限を追加。

--- aws-infra/lib/stage/code-build/code-build-grant-policy-for-cdk-migrate.ts
+++ aws-infra/lib/stage/code-build/code-build-grant-policy-for-cdk-migrate.ts
@@ -1,8 +1,9 @@
 import { Construct } from 'constructs';
 import { Stack } from 'aws-cdk-lib';
-import { Project } from 'aws-cdk-lib/aws-codebuild';
-import { ManagedPolicy, IRole } from 'aws-cdk-lib/aws-iam';
+import { IProject, Project } from 'aws-cdk-lib/aws-codebuild';
+import { ManagedPolicy, IRole, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { CodeBuildGrantPolicyProps } from './code-build-grant-policy';
+import { Context } from '../../context/context';
 
 
 /**
@@ -15,12 +16,15 @@ export class CodeBuildGrantPolicyForCdkMigrate extends Construct {
     constructor(stack: Stack, id: string, props: CodeBuildGrantPolicyProps) {
         super(stack, id);
 
-        this.grantPolicy(props.codeBuildProject);
+        this.grantPolicy(props.codeBuildProject, props, stack);
     }
 
-    private grantPolicy(project: Project): void {
+    private grantPolicy(
+        project: IProject,
+        props: CodeBuildGrantPolicyProps,
+        stack: Stack
+    ) {
         const role = project.role as IRole;
-
         const awsManagedPolicyNames = [
             'AWSCodeBuildAdminAccess', // TODO AWSCodeBuildDeveloperAccess に変えられないか？
             'AmazonECS_FullAccess',
@@ -30,9 +34,16 @@ export class CodeBuildGrantPolicyForCdkMigrate extends Construct {
             'AWSLambda_FullAccess',
             'AmazonDynamoDBFullAccess'
         ];
-
         for (const name of awsManagedPolicyNames) {
             role.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName(name));
         }
+
+        const context = props.context;
+        const me = Stack.of(this).account;
+        role.addToPrincipalPolicy(PolicyStatement.fromJson({
+            "Effect": "Allow",
+            "Action": "ssm:GetParameters",
+            "Resource": `arn:aws:ssm:${stack.region}:${me}:parameter/${context.systemName()}-*`
+        }));
     }
 }
