c2dc7a2646cddbd7afd7de28e525fe36b3c94bdf
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-15 17:22:03 +0900

    implement: スクリプトに「設定ファイルによってはスキップする」ように。

--- cd/deploy/migration_cdk.sh
+++ cd/deploy/migration_cdk.sh
@@ -5,11 +5,26 @@
 #   jq,
 #
 # 前提として必要な環境変数
-#   STAGE_ID, CONTAINER_REGISTRY_URI_APP, ECS_CLUSTER, ECS_SERVICE, ECS_TASK_FAMILY
+#   STAGE_ID
 #
 
 set -eux
 
 VERSION_TAG=${1}
+
+CONTEXT_JSON_FILE_PATH='./aws-infra/cdk.json'
+
+migration_cdk=$(jq ".context.stages.${STAGE_ID}.migrateInfrastructure" ${CONTEXT_JSON_FILE_PATH})
+
+echo ${migration_cdk}
+
+if [[ "${migration_cdk}" != 'true' ]] ; then
+    echo "migrateInfrastructure が ${migration_cdk} であるため、${STAGE_ID} へのCDKの適用はスキップします。"
+    exit 0
+fi
+
+echo "migrateInfrastructure:${migration_cdk} ${STAGE_ID} へのCDKを適用。"
+
+
 
 exit 0
