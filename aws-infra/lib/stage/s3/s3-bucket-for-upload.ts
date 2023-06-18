92f0f8e4fd898011aa8e056073ca31113cf41a0b
kazuhito-m <kazuhito.sumpic@gmail.com>
2023-06-18 17:31:46 +0900

    implement: とりあえず「ダウンロードはし放題」なS3バケットを定義する。

--- aws-infra/lib/stage/s3/s3-bucket-for-upload.ts
+++ aws-infra/lib/stage/s3/s3-bucket-for-upload.ts
@@ -1,22 +1,28 @@
 import { Construct } from 'constructs';
 import { InstanceType, SecurityGroup, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2'
 import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
-import { Credentials, DatabaseInstance, DatabaseInstanceEngine, MysqlEngineVersion, SubnetGroup } from 'aws-cdk-lib/aws-rds';
-import { SecretValue } from 'aws-cdk-lib';
 import { Context } from '../../context/context';
+import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
 
 export interface S3BucketForUploadProps {
     readonly context: Context;
 }
 
 export class S3BucketForUpload extends Construct {
-    public readonly appRds: DatabaseInstance;
-    public readonly rdsSecret: Secret;
-
     constructor(scope: Construct, id: string, props: S3BucketForUploadProps) {
         super(scope, id);
 
         const context = props.context;
 
+        const bucket = new Bucket(this, 'S3CreateBucket', {
+            bucketName: context.s3BucketName(),
+            publicReadAccess: true,
+            blockPublicAccess: new BlockPublicAccess({
+                blockPublicAcls: false,
+                ignorePublicAcls: false,
+                blockPublicPolicy: false,
+                restrictPublicBuckets: false,
+            })
+        });
     }
 }
