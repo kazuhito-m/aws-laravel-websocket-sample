42d080a30053d6e87f40fb982373462e3a5aed75
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-22 20:48:08 +0900

    implement: 本ちゃんConstructに、WAFの仕組みを組み込む。

--- aws-infra/lib/stage/waf/waf-and-authentication.ts
+++ aws-infra/lib/stage/waf/waf-and-authentication.ts
@@ -1,8 +1,6 @@
 import { Construct } from 'constructs';
 import { Context } from '../../context/context';
-import { CfnWebACL } from 'aws-cdk-lib/aws-wafv2';
-import { IResolvable } from 'aws-cdk-lib';
-import { CfnWebACLAssociation } from 'aws-cdk-lib/aws-wafregional';
+import { CfnWebACL, CfnWebACLAssociation } from 'aws-cdk-lib/aws-wafv2';
 import { ApplicationLoadBalancer } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 
 export interface WafAndAuthenticationrops {
@@ -16,30 +14,73 @@ export class WafAndAuthentication extends Construct {
 
         const context = props.context;
 
-        const waf = new CfnWebACL(this, "CreateWAF", {
-            name: context.wpk('waf-web-acl'),
+        const waf = new CfnWebACL(this, "CreateWaf", {
+            name: context.wpk('waf-webacl'),
             defaultAction: { allow: {} },
             scope: 'REGIONAL',
             visibilityConfig: {
                 cloudWatchMetricsEnabled: true,
                 sampledRequestsEnabled: true,
-                metricName: context.wpk('app-wafv2-webacl'),
+                metricName: context.wpk('app-wafv2-webacl-metric'),
             },
             rules: this.buildRules(context),
         });
 
-        const webAclAssociation = new CfnWebACLAssociation(this, "AddAlbResource", {
+        const webAclAssociation = new CfnWebACLAssociation(this, "WafAddAlbResource", {
             resourceArn: props.alb.loadBalancerArn,
-            webAclId: waf.attrArn,
+            webAclArn: waf.attrArn,
         });
         webAclAssociation.addDependency(waf);
     }
 
-    private buildRules(context: Context): Array<CfnWebACL.RuleProperty | IResolvable> {
-        const rules = Array();
+    private buildRules(context: Context): Array<CfnWebACL.RuleProperty> {
+        const rules = Array<CfnWebACL.RuleProperty>();
 
-
+        rules.push(this.basicAuthorizationRule());
 
         return rules;
+    }
+
+    private basicAuthorizationRule(): CfnWebACL.RuleProperty {
+        const userAndPass = 'testuser:testpass';
+        const encodedUP = Buffer.from(userAndPass).toString('base64');
+
+        return {
+            name: 'basic-authorization-rule',
+            priority: 0,
+            statement: {
+                notStatement: {
+                    statement: {
+                        byteMatchStatement: {
+                            searchString: `Basic ${encodedUP}`,
+                            fieldToMatch: {
+                                singleHeader: { name: 'authorization' }
+                            },
+                            textTransformations: [{
+                                priority: 0,
+                                type: 'NONE'
+                            }],
+                            positionalConstraint: 'EXACTLY',
+                        }
+                    }
+                }
+            },
+            action: {
+                block: {
+                    customResponse: {
+                        responseCode: 401,
+                        responseHeaders: [{
+                            name: 'www-authenticate',
+                            value: 'Basic'
+                        }]
+                    }
+                }
+            },
+            visibilityConfig: {
+                sampledRequestsEnabled: true,
+                cloudWatchMetricsEnabled: false,
+                metricName: "BasicAuthRule"
+            }
+        }
     }
 }
