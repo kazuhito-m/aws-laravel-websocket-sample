077b90b434655b26f85c834d996f3c0f21152ad1
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-22 23:34:08 +0900

    refactor: StrorageクラスでDynamoDB周りを置き換え。

--- webapp/app/Http/Controllers/DirectSendController.php
+++ webapp/app/Http/Controllers/DirectSendController.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Websocket\WebsocketConnectionStorage;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\DirectSend\ClientPushSignalForWebSocketEndpoint;
-use App\Models\Websocket\WebsocketConnectionDDB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
 use Aws\ApiGatewayManagementApi\ApiGatewayManagementApiClient;
-use Aws\DynamoDb\DynamoDbClient;
 
 class DirectSendController extends Controller
 {
@@ -36,31 +35,11 @@ class DirectSendController extends Controller
 
     private function sendMessageOf(string $id, string $message)
     {
+        $storage = WebsocketConnectionStorage::of();
+
         Log::info('ID:' . $id . ', message:' . $message);
 
-        $client = $this->createDynamoDBClient();
-
-        $tableName = config('custom.wsddb-table-name');
-        Log::info('検索対象のDynamoDBのテーブル名:' . $tableName);
-
-        $records = $client->scan(['TableName' => $tableName]);
-
-        $websocketConnections = array();
-        foreach ($records['Items'] as $record) {
-            $connection = WebsocketConnectionDDB::of(
-                $record['connectionId']['S'],
-                $record['userId']['S'],
-                $record['connectedTime']['S'],
-            );
-            array_push($websocketConnections, $connection);
-        }
-
-        $connectionIds = array();
-        foreach ($websocketConnections as $connection) {
-            if ($connection->userId == $id) {
-                array_push($connectionIds, $connection->connectionId);
-            }
-        }
+        $connectionIds = $storage->findConnectionIdsOf($id);
 
         Log::debug('connextionIds');
         Log::debug($connectionIds);
@@ -88,31 +67,6 @@ class DirectSendController extends Controller
                 'Data' => json_encode($signal),
             ]);
         }
-    }
-
-    private function createDynamoDBClient()
-    {
-        $config = [
-            'region' => config('custom.websocket-api-region'),
-            'version' => 'latest',
-            'http' => [
-                'timeout' => 5,
-            ],
-        ];
-
-        $accessKey = config('custom.wsddb-aws-access-key-id');
-        $secretKey = config('custom.wsddb-aws-secret-access-key');
-        if (!(empty($accessKey) && empty($secretKey))) {
-            $config['credentials'] = [
-                'key' => $accessKey,
-                'secret' => $secretKey
-            ];
-            Log::info('Credential情報在りでDynamoDbClient生成。accessKey:' . $accessKey . ', $secretKey:' . $secretKey);
-        } else {
-            Log::info('Credential情報無しでDynamoDbClient生成。');
-        }
-
-        return new DynamoDbClient($config);
     }
 
     private function completingTrailingSlashesOf(string $text)
