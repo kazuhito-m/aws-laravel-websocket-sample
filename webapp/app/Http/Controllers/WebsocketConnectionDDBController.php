3b1a4d151abcc0fdc9d4eebe929276a019c3d850
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-22 23:38:00 +0900

    refactor: コントローラのDynamoDBアクセス部分をWebsocketConnectionStorageで置き換え。

--- webapp/app/Http/Controllers/WebsocketConnectionDDBController.php
+++ webapp/app/Http/Controllers/WebsocketConnectionDDBController.php
@@ -2,28 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Websocket\WebsocketConnectionDDB;
-use Illuminate\Support\Facades\Log;
-
-use Aws\DynamoDb\DynamoDbClient;
+use App\Models\Websocket\WebsocketConnectionStorage;
 
 class WebsocketConnectionDDBController extends Controller
 {
     public function index()
     {
-        $client = $this->createDynamoDBClient();
+        $storage = WebsocketConnectionStorage::of();
 
-        $records = $client->scan(['TableName' => $this->ddTableName()]);
-
-        $websocketConnections = array();
-        foreach ($records['Items'] as $record) {
-            $connection = WebsocketConnectionDDB::of(
-                $record['connectionId']['S'],
-                $record['userId']['S'],
-                $record['connectedTime']['S'],
-            );
-            array_push($websocketConnections, $connection);
-        }
+        $websocketConnections = $storage->findAll();
 
         return view('websocketconnectionddb.index')
             ->with('websocketConnections', $websocketConnections);
@@ -31,45 +18,11 @@ class WebsocketConnectionDDBController extends Controller
 
     public function destroy(string $connectionId)
     {
-        Log::debug('削除対象ID:' . $connectionId);
-        $client = $this->createDynamoDBClient();
+        $storage = WebsocketConnectionStorage::of();
 
-        $client->deleteItem([
-            'Key' => ['connectionId' => ['S' => $connectionId]],
-            'TableName' => $this->ddTableName(),
-        ]);
+        $storage->removeOf($connectionId);
 
         return redirect()->route('websocketconnectionsddb.index')
             ->with('success', 'websocket connections deleted successfully');
-    }
-
-    private function createDynamoDBClient()
-    {
-        $config = [
-            'region' => config('custom.websocket-api-region'),
-            'version' => 'latest',
-            'http' => [
-                'timeout' => 5,
-            ],
-        ];
-
-        $accessKey = config('custom.wsddb-aws-access-key-id');
-        $secretKey = config('custom.wsddb-aws-secret-access-key');
-        if (!(empty($accessKey) && empty($secretKey))) {
-            $config['credentials'] = [
-                'key' => $accessKey,
-                'secret' => $secretKey
-            ];
-            Log::info('Credential情報在りでDynamoDbClient生成。accessKey:' . $accessKey . ', $secretKey:' . $secretKey);
-        } else {
-            Log::info('Credential情報無しでDynamoDbClient生成。');
-        }
-
-        return new DynamoDbClient($config);
-    }
-
-    private function ddTableName()
-    {
-        return config('custom.wsddb-table-name');
     }
 }
