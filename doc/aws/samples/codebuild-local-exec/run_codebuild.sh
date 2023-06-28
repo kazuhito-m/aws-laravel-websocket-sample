48dd0305c49527ef8f7754a39e50f80c56843a79
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-29 03:15:42 +0900

    implement: 「ローカルでAWS CodeBuildを実行する」一式のサンプルを作成。

---
+++ doc/aws/samples/codebuild-local-exec/run_codebuild.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+REPO_DIR=aws-codebuild-docker-images
+CODEBUILD_EXEC_CONTAINER_IMAGE_TAG=aws/ubuntu-standard:6.0
+
+# 「このスクリプトがある場所」まで移動
+SCRIPT_DIR=$(cd $(dirname $(readlink -f $0 || echo $0));pwd -P)
+cd ${SCRIPT_DIR}
+
+# initialize
+rm -rf ./${REPO_DIR}
+
+# main
+
+git clone https://github.com/aws/${REPO_DIR}.git
+
+pushd ./${REPO_DIR}/ubuntu/standard/6.0
+
+sed -ie 's/^ENTRYPOINT/#ENTRYPOINT/g' Dockerfile
+docker build -t ${CODEBUILD_EXEC_CONTAINER_IMAGE_TAG} .
+
+popd
+
+docker pull amazon/aws-codebuild-local:latest --disable-content-trust=false
+
+docker run \
+  -it -v /var/run/docker.sock:/var/run/docker.sock \
+  -e "IMAGE_NAME=${CODEBUILD_EXEC_CONTAINER_IMAGE_TAG}" \
+  -e "SOURCE=$(pwd)" \
+  amazon/aws-codebuild-local
