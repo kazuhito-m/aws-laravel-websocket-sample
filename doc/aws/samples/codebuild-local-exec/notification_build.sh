cd32ad8e2595d3eb86d53763711846de81fc937f
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-06-29 06:29:24 +0900

    implement: 試しにSlack通知の部分を試作してみる。

---
+++ doc/aws/samples/codebuild-local-exec/notification_build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# 「Tagを切ったことによるDockerImageのビルド結果」をSlackへ投稿するスクリプト。
+#
+# Premise
+#   以下の環境変数は呼び出し元(CodeBuild or 以前の処理)で設定され、渡ってくる前提
+#     SLACK_WEBHOOK_URL, IMAGE_TAG, AWS_DEFAULT_REGION, CODEBUILD_WEBHOOK_ACTOR_ACCOUNT_ID, CODEBUILD_SOURCE_REPO_URL
+#
+# Usage
+#   notification_build.sh "true"|"false"
+
+SCRIPT_DIR=$(cd $(dirname $(readlink -f $0 || echo $0));pwd -P)
+cd ${SCRIPT_DIR}
+
+RESULT=${1}
+
+color='danger'
+result_text='失敗'
+if [ $RESULT = 'true']; then
+    color='good'
+    result_text='完了'
+fi
+
+ecr_console="https://${AWS_DEFAULT_REGION}.console.aws.amazon.com/ecr/repositories/private/${CODEBUILD_WEBHOOK_ACTOR_ACCOUNT_ID}/alws-app"
+
+data=`cat <<_EOT_
+{
+     "attachments": [
+        {
+	        "mrkdwn_in": ["text"],
+            "color": "${color}",
+            "pretext": "maho.cityのコンテナイメージのビルドと登録が${result_text}しました。",
+            "title": "CodeBuild:${CODEBUILD_BUILD_ID}",
+            "title_link": "${CODEBUILD_PUBLIC_BUILD_URL}",
+            "fields": [
+                {
+                    "title": "ContainerTag/Version",
+                    "value": "${IMAGE_TAG}",
+                    "short": false
+                },
+                {
+                    "title": "ECR(AWS Console):alws-app",
+                    "value": "${ecr_console}",
+                    "short": false
+                },
+                {
+                    "title": "Source",
+                    "value": "${CODEBUILD_SOURCE_REPO_URL}",
+                    "short": false
+                }
+            ]
+        }
+    ]
+}
+_EOT_`
+
+./slackSend.sh "${data}" ${SLACK_WEBHOOK_URL}
+
